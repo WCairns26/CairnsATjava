e5ba553ee8f0cfa476b33b496294ed015ac2e0e1
WCairns26 <26wcairns@derryfield.org>
2026-02-20 15:27:52 -0500

    Add files via upload

---
+++ Unit07Practical/Unit07Test.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Unit07Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        File iFile = new File("thisFile.txt");
+        Scanner in = new Scanner(iFile);
+        double sum = 0.0;
+        int scoresNum = 0;
+        System.out.println("Name               | School                  | Score");
+        System.out.println("------------------------------------------------------");
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            int firstCom = line.indexOf(",");
+            int secondCom = line.indexOf(",", firstCom + 1);
+            String name = line.substring(0, firstCom);
+            String school = line.substring(firstCom + 1, secondCom);
+            String stringScore = line.substring(secondCom + 1);
+            double score = Double.parseDouble(stringScore);
+            sum = sum + score;
+            scoresNum = scoresNum + 1;
+            System.out.print(name + "    | ");
+            System.out.print(school + "    | ");
+            System.out.printf("%.1f%n", score);
+        }
+        in.close();
+        System.out.println("------------------------------------------------------");
+        double avg = sum / scoresNum;
+        System.out.printf("Average Score: %.1f%n", avg);
+    }
+}
